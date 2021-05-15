5e28555a663d2930eccd91255934390edc16aa00
Fredy <fredy.huachaca21@gmail.com>
2021-05-15 16:08:29 -0500

    First Commit

---
+++ src/main/java/com/pe/fhuachaca/app/springreactor/model/Plato.java
@@ -0,0 +1,8 @@
+package com.pe.fhuachaca.app.springreactor.model;
+
+public class Plato {
+
+    private String id;
+    private String nombre;
+    private String estado;
+}
---
+++ src/test/java/com/pe/fhuachaca/app/springreactor/SpringReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.fhuachaca.app.springreactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
